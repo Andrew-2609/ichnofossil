41009d36afdf2b605f01d919a39d5e417375019e
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-04-28 22:10:40 -0300

    form to create new Posts added (view and controller), tests for it created

--- src/main/java/com/ndrewcoding/ichnofossil/controller/PostController.java
+++ src/main/java/com/ndrewcoding/ichnofossil/controller/PostController.java
@@ -5,12 +5,17 @@ import com.ndrewcoding.ichnofossil.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -20,7 +25,7 @@ public class PostController {
 
     private final PostService postService;
 
-    @GetMapping()
+    @GetMapping
     public ModelAndView getPosts() {
         ModelAndView modelAndView = new ModelAndView("posts");
         List<Post> posts = postService.findAll();
@@ -34,5 +39,20 @@ public class PostController {
         Post post = postService.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
         modelAndView.addObject("post", post);
         return modelAndView;
+    }
+
+    @GetMapping("/new")
+    public String getPostForm() {
+        return"postForm";
+    }
+
+    @PostMapping(value = "/new")
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+        if(result.hasErrors()) {
+            return "redirect:/new";
+        }
+        post.setReleaseDate(LocalDate.now());
+        postService.save(post);
+        return "redirect:/posts";
     }
 }
--- src/test/java/com/ndrewcoding/ichnofossil/controller/PostControllerTest.java
+++ src/test/java/com/ndrewcoding/ichnofossil/controller/PostControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -41,7 +42,7 @@ public class PostControllerTest {
     @Test
     @DisplayName("Must return all the Posts in the view")
     public void getPostsTest() throws Exception {
-        List<Post> posts = createNewPost();
+        List<Post> posts = createNewPostList();
 
         BDDMockito.given(postService.findAll()).willReturn(posts);
 
@@ -61,7 +62,7 @@ public class PostControllerTest {
     @Test
     @DisplayName("Must return a Post view with its given ID")
     public void getPostDetailsTest() throws Exception {
-        Post post = createNewPost().get(0);
+        Post post = createNewPostList().get(0);
 
         Long generic_id = 1L;
 
@@ -80,7 +81,50 @@ public class PostControllerTest {
                 .andExpect(content().string(containsString(post.getReleaseDate().toString())));
     }
 
-    private List<Post> createNewPost() {
+    @Test
+    @DisplayName("Must return 404 when Post is not found")
+    public void getNonexistentPostDetailsTest() throws Exception {
+        BDDMockito.given(postService.findById(Mockito.anyLong())).willReturn(Optional.empty());
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .get(POSTS_URL.concat("/" + 1L))
+                .accept(MediaType.TEXT_HTML);
+
+        mvc.perform(request).andExpect(status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("Must return the New Post view")
+    public void getPostFormTest() throws Exception {
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .get(POSTS_URL.concat("/new"))
+                .accept(MediaType.TEXT_HTML);
+
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(view().name("postForm"));
+    }
+
+    @Test
+    @DisplayName("Must successfully create a new Post")
+    public void createPostTest() throws Exception {
+        Post post = createNewPostList().get(0);
+
+        BDDMockito.given(postService.save(Mockito.any(Post.class))).willReturn(post);
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post(POSTS_URL.concat("/new"))
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                .param("title", post.getTitle())
+                .param("author", post.getAuthor())
+                .param("text", post.getText());
+
+        mvc.perform(request)
+                .andExpect(status().isFound())
+                .andExpect(view().name("redirect:/posts"));
+    }
+
+    private List<Post> createNewPostList() {
         return List.of(Post.builder().id(1L).author("Andrew").title("My Studies").releaseDate(LocalDate.now()).text("Content").build());
     }
 
