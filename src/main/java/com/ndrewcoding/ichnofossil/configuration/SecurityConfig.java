3a9ef365781f0e95126842b6c798eb7eb582fc76
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-04-30 22:52:07 -0300

    routes refactored, Spring Security for inMemoryAuthentication configured

---
+++ src/main/java/com/ndrewcoding/ichnofossil/configuration/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.ndrewcoding.ichnofossil.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String[] AUTH_LIST = {"/", "/posts", "/posts/{id}"};
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable().authorizeRequests()
+                .antMatchers(AUTH_LIST).permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin().permitAll()
+                .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("ndrewcoding").password(passwordEncoder().encode("ichnofossil")).roles("ADMIN");
+    }
+}
--- src/main/java/com/ndrewcoding/ichnofossil/controller/PostController.java
+++ src/main/java/com/ndrewcoding/ichnofossil/controller/PostController.java
@@ -9,7 +9,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -19,13 +18,12 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Controller
-@RequestMapping("/posts")
 @RequiredArgsConstructor
 public class PostController {
 
     private final PostService postService;
 
-    @GetMapping
+    @GetMapping("/posts")
     public ModelAndView getPosts() {
         ModelAndView modelAndView = new ModelAndView("posts");
         List<Post> posts = postService.findAll();
@@ -33,7 +31,7 @@ public class PostController {
         return modelAndView;
     }
 
-    @GetMapping("{id}")
+    @GetMapping("/posts/{id}")
     public ModelAndView getPostDetails(@PathVariable("id") Long id) {
         ModelAndView modelAndView = new ModelAndView("postDetails");
         Post post = postService.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
@@ -49,6 +47,8 @@ public class PostController {
     @PostMapping(value = "/new")
     public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
         if(result.hasErrors()) {
+            attributes.addFlashAttribute("message", "Check if the required fields are filled.");
+            //noinspection SpringMVCViewInspection
             return "redirect:/new";
         }
         post.setReleaseDate(LocalDate.now());
