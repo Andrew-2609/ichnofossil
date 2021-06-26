0dcac8126925ce8d7799be4a980f9b1d92eadc51
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-06-25 23:04:30 -0300

    custom erros implemented

---
+++ src/main/java/com/ndrewcoding/ichnofossil/controller/MyErrorController.java
@@ -0,0 +1,37 @@
+package com.ndrewcoding.ichnofossil.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class MyErrorController implements ErrorController {
+
+    @GetMapping("/error")
+    public String handleError(HttpServletRequest request) {
+
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "error/error-404";
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "error/error-500";
+            }
+        }
+
+        return "error/error";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return null;
+    }
+
+}
